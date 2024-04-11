0332c3334c7b217ed0b93b8b42a0db41c264906b
WebFan37 <jeanneel2000@gmail.com>
2024-04-11 10:17:11 -0400

    INTRA - Update titre du thème.  Insert image et organiser la page 404

--- 404.php
+++ 404.php
@@ -2,29 +2,37 @@
 
     <div id="page-erreur" class="global diagonal">
         <section>
-            <h2>Erreur 404</h2>
 
-            <p>
-                Vous essayez d'accéder à une page qui n'existe pas.
-            </p>
+            <div class="erreur">
+                <h2>Erreur 404</h2>
 
-            <p>
-                Pour revenir à la page d'accueil cliquer sur le lien suivant
-            </p>
+                <p>
+                    Vous essayez d'accéder à une page qui n'existe pas.
+                </p>
+
+                <p>
+                    Pour revenir à la page d'accueil cliquer sur le lien suivant
+                </p>
+                
+                <!--NAV MENU SIMPLE RETOUR ARRIER-->
+                <?php wp_nav_menu(array("menu" => "return-menu", "container" => "nav")); ?>
+
+                <!--SEARCH FORM-->
+                <?php get_search_form() ?>
+
+                <!--NAV MENU CATEGORY-->
+                <?php wp_nav_menu(array("menu" => "menu-category", "container" => "nav")); ?>
+
+
+                <h3>
+                <a href="<?php echo get_bloginfo("URL"); ?>"><?php echo get_bloginfo("name"); ?></a>
+                </h3>
+            </div>
+
+            <div class="image">
+                <img src="images/logo-page404.png" alt="travel logo with a plane">
+            </div>
             
-            <!--NAV MENU SIMPLE RETOUR ARRIER-->
-            <?php wp_nav_menu(array("menu" => "return-menu", "container" => "nav")); ?>
-
-            <!--SEARCH FORM-->
-            <?php get_search_form() ?>
-
-            <!--NAV MENU CATEGORY-->
-            <?php wp_nav_menu(array("menu" => "menu-category", "container" => "nav")); ?>
-
-
-            <h3>
-            <a href="<?php echo get_bloginfo("URL"); ?>"><?php echo get_bloginfo("name"); ?></a>
-        </h3>
        
        
 
