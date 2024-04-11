17fb895fd688c2fd1fe850dd380cb3cae543cedf
WebFan37 <jeanneel2000@gmail.com>
2024-04-11 09:31:27 -0400

    INTRA - Ajouter textes  et search button de la maquette

--- 404.php
+++ 404.php
@@ -1,15 +1,23 @@
 <?php get_header(); ?>
 
-    <div id="test-erreur" class="global diagonal">
+    <div id="page-erreur" class="global diagonal">
         <section>
-            <h2>Accueil Nope pas ici</h2>
+            <h2>Erreur 404</h2>
+
+            <p>
+                Vous essayez d'accéder à une page qui n'existe pas.
+            </p>
+
+            <p>
+                Pour revenir à la page d'accueil cliquer sur le lien suivant
+            </p>
+
+            <?php get_search_form() ?>
+
             <h3>
-            
-            
             <a href="<?php echo get_bloginfo("URL"); ?>"><?php echo get_bloginfo("name"); ?></a>
         </h3>
-       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eos ad odit eius nulla quas voluptates tempora illum! Architecto blanditiis neque animi fugit quos nemo odio omnis ex praesentium modi?</p>
-       <a href="https://www.shapedivider.app/">Créer des formes personnalisé</a>
+       
        
 
         </section>
