1e61c59a7688977feae387e9e6c015fcb614709d
WebFan37 <jeanneel2000@gmail.com>
2024-04-25 12:29:06 -0400

    Putting thumbnail on search result

--- search.php
+++ search.php
@@ -26,6 +26,7 @@
                     ?>
                     <div class="carte">
                         <h2><?php echo $titre ?></h2>
+                        <h2><?php the_post_thumbnail("medium")?></h2>
                         <p><?php echo wp_trim_words(get_the_content(), 50); ?></p>
                         <a href="<?php echo get_permalink(); ?>">SUITE ...</a>
                                 
