39995983dbcab460a0ce4034f5b30be77ade25c0
WebFan37 <jeanneel2000@gmail.com>
2024-05-09 10:38:52 -0400

    Footer main: change Author and desc color. Word syntax correction

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
             <p>Voici des liens pour aller voir dans chaque catégories différentes</p>
             <div class="menu-fin">
                 <ul>
-                    <p>D'AUTRES AGENCE DE VOAYAGE</p>
+                    <p>D'AUTRES AGENCES DE VOYAGE</p>
                     <li><a href="https://www.ncl.com/ca/en/?cid=PS_DIG_NA_BRC_GOO_NA_NA_NA_NA_NA_NAM&gad_source=1&gclid=CjwKCAjw_LOwBhBFEiwAmSEQAXLWt27nOzmO-9iWAqVvapHCT_Rxm29M5fRtg4XAT7k8FJ342qh7HBoCJ5MQAvD_BwE&gclsrc=aw.ds">CROISIÈRE NORWEGIAN</a></li>
                     <li><a href="https://www.orbitz.com/">VOYAGE ORBITZ</a></li>
                     <li><a href="https://www.orbitz.com/">VOYAGE AIR CANADA</a></li>
@@ -20,9 +20,9 @@
                   
                 </ul>
                 <ul>
-                    <li><a href="#">Janne Zhang Ellison</a></li>
                     <li><a href="https://github.com/WebFan37/4W4-2024-gr1/tree/TP2">Addresse GitHub TP2</a></li>
-                    <li><p>DESCRIPTION: Ce Travail Pratique consiste un pratique dans l'application du SCSS, php 
+                    <li><p><span>AUTEUR</span>: Janne Zhang Ellison</p></li>
+                    <li><p><span>DESCRIPTION</span>: Ce Travail Pratique consiste un pratique dans l'application du SCSS, php 
                         WordPress et les fonctionnalités du WordPress</p></li>
     
                 </ul>
