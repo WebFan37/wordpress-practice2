5838900ce515a65033a146182832c82e638fbe68
WebFan37 <jeanneel2000@gmail.com>
2024-05-16 11:40:30 -0400

    Footer add more logo and links & css correction. Links css reset

--- footer.php
+++ footer.php
@@ -12,9 +12,15 @@
                 </ul>
                 <ul>
                     <li><?php get_search_form() ?></li>
-                    <li><a href="https://www.cmaisonneuve.qc.ca/campus-installations/trois-campus/3800-rue-sherbrooke/">Addresse: 3800 Rue Sherbrooke Est</a></li>
-                    <li><a href="https://www.facebook.com/CollegeMaisonneuve/?locale=fr_CA"><img src="https://s2.svgbox.net/materialui.svg?ic=facebook" width="28" height="28"></a></li>
-                    <li><a href="https://www.instagram.com/collegemaisonneuve/"><img src="https://s2.svgbox.net/social.svg?ic=instagram&color=000" width="28" height="28"></a></li>
+                    <li><a href="https://www.cmaisonneuve.qc.ca/campus-installations/trois-campus/3800-rue-sherbrooke/"><img src="https://s2.svgbox.net/materialui.svg?ic=place&color=000" width="32" height="32" color="000">3800 Rue Sherbrooke Est</a></li>
+                    <li class="logo-social">
+                        <a href="https://www.facebook.com/CollegeMaisonneuve/?locale=fr_CA"><img src="https://s2.svgbox.net/materialui.svg?ic=facebook" width="32" height="32"></a>
+                        <a href="https://www.instagram.com/collegemaisonneuve/"><img src="https://s2.svgbox.net/social.svg?ic=instagram&color=000" width="32" height="32"></a>
+                        <a href="https://www.youtube.com/channel/UCIXtOLJmwNPBUuR-sGm7VSw"><img src="https://s2.svgbox.net/social.svg?ic=youtube&color=000" width="32" height="32"></a>
+                        <a href="https://twitter.com/CdeMaisonneuve"><img src="https://s2.svgbox.net/social.svg?ic=twitter&color=000" width="32" height="32"></a>
+                        <a href="https://www.linkedin.com/school/collegedemaisonneuve/"><img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000" width="32" height="32"></a>
+                    </li>
+                    
                   
                 </ul>
                 <ul>
