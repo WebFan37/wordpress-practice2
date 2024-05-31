9fca4943292fff978c0e55a5a724df002a5ea4b1
WebFan37 <jeanneel2000@gmail.com>
2024-05-31 15:22:30 -0400

    Interruption reprise besoin

---
+++ template-pays.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<!-- <h1>FRONT-PAGE TEST</h1> -->
+
+    <div id="pays" class="pays">
+        <section>
+            <h2>TEST</h2> 
+            
+            <div class="pays">
+               
+            </div>
+        </section>
+        
+    </div>
+   
+    <div id="galerie" class="global">
+        <section>
+           
+        </section>
+        
+    </div>
+
+    <?php get_footer(); ?>
+  
