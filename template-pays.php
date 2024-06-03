9289ed995061634b10e160217b916a5611840eb5
WebFan37 <jeanneel2000@gmail.com>
2024-06-03 16:50:57 -0400

    Padding sur page pays. Activer plugin. Changer le nom a pays.php

--- template-pays.php
+++ template-pays.php
@@ -11,14 +11,14 @@
         <?php
             if ( have_posts() ) : the_post(); ?>
             <h1><?= get_the_title(); ?></h1>
-            <?php the_content();?>
-            <p>L'adresse de l'evénement: <?php the_field('adresse'); ?></p>
-            <p>La date et l'heure de l'événement: <?php the_field('date_et_heure'); ?></p>     
+            <?php the_content();?>   
             <?php endif;?>
 
         <section class="presentation_pays">
-        <?php get_template_part('composants-gabarit/vague_pays'); ?>
+        <?php get_template_part('composants-gabarit/pays'); ?>
 
+
+        <?php  echo do_shortcode('[em_pays]'); ?>
         </section>
 
     </main><!-- #main -->
